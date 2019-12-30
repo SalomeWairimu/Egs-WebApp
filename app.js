34669c070ecba1e029b63f8b29f2214b8f84b583
SalomeWairimu <salomekariuki2020@u.northwestern.edu>
2019-12-30 10:35:25 +0000

    exec page done

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var scholarRoutes = require("./routes/scholars");
 
 // mongoose.connect("mongodb://localhost:27017/egsdemo",{useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false });
 mongoose.connect("mongodb+srv://egs:Egs2019@cluster0-lgdws.mongodb.net/test?retryWrites=true",{
-    useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false
+    useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true
     }).then(()=> {
         console.log("connected");
     }).catch(err => {
